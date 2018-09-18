cebbe0ec28773fc8e375dbc338e2d5d67ec9d170
Nicholas Baltodano <nicholasbaltodano@gmail.com>
2018-09-17 23:03:20 -0500

    Web Initializer / Setting up servlet registrations

--- src/main/java/nicholas/baltodano/config/webAppInitializer.java
+++ src/main/java/nicholas/baltodano/config/webAppInitializer.java
@@ -1,22 +1,36 @@
 package nicholas.baltodano.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.proxy.Dispatcher;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 @Slf4j
 public class webAppInitializer implements WebApplicationInitializer {
 
+    private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
 
     @Override
-
     public void onStartup(ServletContext servletContext) throws ServletException {
         log.info("onStartup Called");
 
         // Create the Spring Application context
-        AnnotationConfigWebApplicationContext  context = new AnnotationConfigWebApplicationContext();
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+
+        context.register(WebConfig.class);
+
+        // Create dispatcher servlet
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+
+        // Register and Configure the dispatcher servlet
+        ServletRegistration.Dynamic registration = servletContext.addServlet(DISPATCHER_SERVLET_NAME, dispatcherServlet);
+
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
     }
 }
