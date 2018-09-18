113fedceff43cd634c699d9e79c825530a081d33
Nicholas Baltodano <nicholasbaltodano@gmail.com>
2018-09-17 22:28:09 -0500

    Add Spring app context

---
+++ src/main/java/nicholas/baltodano/config/webAppInitializer.java
@@ -0,0 +1,22 @@
+package nicholas.baltodano.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+@Slf4j
+public class webAppInitializer implements WebApplicationInitializer {
+
+
+    @Override
+
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        log.info("onStartup Called");
+
+        // Create the Spring Application context
+        AnnotationConfigWebApplicationContext  context = new AnnotationConfigWebApplicationContext();
+    }
+}
