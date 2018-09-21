cc8656a4b7962e40de64b4905eebad105d3b0225
Nicholas Baltodano <nicholasbaltodano@gmail.com>
2018-09-20 23:24:04 -0500

    Added bootstrap to the welcome page. Will work on that more later.
    Added services to the project.

--- src/main/java/nicholas/baltodano/controller/DemoController.java
+++ src/main/java/nicholas/baltodano/controller/DemoController.java
@@ -1,16 +1,30 @@
 package nicholas.baltodano.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import nicholas.baltodano.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
 public class DemoController {
 
+    // Fields
+    private final DemoService demoService;
+
+    // Constructors
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
+
+
+    // Request Methods
     // http://localhost:8080/todo-list/hello
     @ResponseBody
     @GetMapping("/hello")
@@ -19,16 +33,22 @@ public class DemoController {
     }
 
     // http://localhost:8080/todo-list/welcome
+    // http://localhost:8080/todo-list/welcome?user=???
     @GetMapping("welcome")
-    public String welcome(Model model){
-        model.addAttribute("user", "Nicholas");
+    public String welcome(@RequestParam String user,@RequestParam int age,  Model model){
+        model.addAttribute("helloMessage", demoService.getHelloMessage(user));
+        model.addAttribute("age", age);
         log.info("model = {}", model);
         return "welcome";
     }
 
+    // Model Attributes
     @ModelAttribute("welcomeMessage")
     public String welcomeMessage(){
         log.info("Welcome Message called");
-        return "welcome to my humble abode";
+        return demoService.getWelcomeMessage();
     }
+
+
+
 }
---
+++ src/main/java/nicholas/baltodano/service/DemoService.java
@@ -0,0 +1,9 @@
+package nicholas.baltodano.service;
+
+
+public interface DemoService {
+
+    public String getHelloMessage(String user);
+
+    public String getWelcomeMessage();
+}
